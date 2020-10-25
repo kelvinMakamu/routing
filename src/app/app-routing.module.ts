70fef86bb3c06c2b035a57933be7c4ee29479494
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-10-25 15:13:00 +0300

    Adds route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,21 @@ import { BlogComponent } from './components/blog/blog.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
 import { UserNotFoundComponent } from './components/user-not-found/user-not-found.component';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 const routes: Routes = [
   { path: 'home',  component: HomeComponent  },
   { path: 'about', component: AboutComponent },
-  { path: 'blog',  component: BlogComponent  },
-  { path: 'users', component: UsersComponent, children: [
+  { path: 'blog',  component: BlogComponent, canActivate:[AuthGuard] },
+  { path: 'users', component: UsersComponent, canActivateChild:[AuthGuard], children: [
     { path : ':id', component: UserDetailComponent },
-    { path : '', component: UserNotFoundComponent }
+    // { path : '', component: UserNotFoundComponent }
   ] },
   { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
+  providers: [AuthGuard],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanDeactivate, CanActivateChild } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return false;
+  }
+  
+}
