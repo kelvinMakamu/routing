7b5fe0e72094752516c217786290a1564e5c319d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-10-26 23:58:26 +0300

    Get user posts logic

--- src/app/components/users/user.service.ts
+++ src/app/components/users/user.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { User } from '../../interfaces/user';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +18,30 @@ export class UserService {
   
   getUsers(): Observable<any>{
     let headers  = new HttpHeaders().set('Authorization','Bearer your-auth-token-here');
-    return this.httpClient.get<any>(this._usersURL, { headers });
+    return this.httpClient.get<any>(this._usersURL, { headers })
+    .map(users => {
+      return users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          username: user.username,
+          phone: user.phone,
+          street: user.address.street,
+          suite: user.address.suite
+        }
+      })
+    });
   }
 
   getUserById(id: number): Observable<any>{
     let headers  = new HttpHeaders().set('Authorization','Bearer your-auth-token-here');
-    return this.httpClient.get<any>(`${this._usersURL}/${id}`,{ headers });
+    return this.httpClient.get<any>(`${this._usersURL}/${id}`,{ headers })
+    .retry(3)
+    .catch(err => {
+      console.log("Gotten an error", err);
+      return err;
+    });
   }
 
   createUser(user: any): Observable<any>{
@@ -39,8 +60,9 @@ export class UserService {
   }
 
   getUserPosts(id: number): Observable<any>{
+    let headers  = new HttpHeaders().set('Authorization','Bearer your-auth-token-here');
     let params = new HttpParams().set('userId',id.toString());
-    return this.httpClient.get<any>(this._postURL,{ params });
+    return this.httpClient.get<any>(this._postURL,{ headers, params });
   }
 
   constructor(private httpClient: HttpClient) { }
